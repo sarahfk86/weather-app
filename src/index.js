ea5da6cdfd27883f8f2544fd82fd27d8aba1340f
sarahfk86 <142462673+sarahfk86@users.noreply.github.com>
2023-09-08 10:05:56 -0400

    forecast addition
    updated js and html to take out unused elements, add forecast panel, put in the javascript to make it work.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,57 @@
-let now = new Date();
-let Days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-let day = Days[now.getDay()];
-let date = now.getDate();
-let Months = [
-  "jan",
-  "feb",
-  "mar",
-  "apr",
-  "may",
-  "jun",
-  "jul",
-  "aug",
-  "sep",
-  "oct",
-  "nov",
-  "dec",
-];
-let month = Months[now.getMonth()];
-let year = now.getFullYear();
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} @ ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
 }
-let seconds = now.getSeconds();
-if (seconds < 10) {
-  seconds = `0${seconds}`;
-}
-let formattedTime = `${hour}:${minutes}:${seconds}`;
-let todaylessDate = `${day}, ${month} ${date}, ${year}`;
-let localTime = document.querySelector("#date-time");
-function updateTime(event) {
-  event.preventDefault();
-  localTime.innerHTML = `${todaylessDate} @ ${formattedTime}`;
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class = "row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class = "col-2">
+    <div class = "weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+    <img src="http://openweathermap.org/img/wn/${
+      forecastDay.weather[0].icon
+    }@2x.png" alt="" width="40"/>
+    <div class = "weather-forecast-temperatures">
+    <span class = "weather-forecast-temperature-max"> ${Math.round(
+      forecastDay.temp.max
+    )}°</span>
+    <span class = "weather-forecast-temperature-min"> ${Math.round(
+      forecastDay.temp.min
+    )}°</span></div></div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 let updater = document.querySelector("#city-search");
 updater.addEventListener("submit", updateTime);
@@ -60,6 +76,7 @@ function getWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 function searchCity(city) {
   let apiKey = "75bb0c1161e91d5869fbb965d8219152";
@@ -75,33 +92,10 @@ function submitSearch(event) {
 }
 cityForm.addEventListener("submit", submitSearch);
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temp-element");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature = celsiusTemperature * (9 / 5) + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temp-element");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
 let celsiusTemperature = null;
 
 let form = document.querySelector("#city-search");
 form.addEventListener("submit", submitSearch);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 function searchLocation(position) {
   let apiKey = "75bb0c1161e91d5869fbb965d8219152";
